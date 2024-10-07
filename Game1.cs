fde3e23b18342d31bbe817d3163196857e5af939
Christian Moyes <339463846@GoTVDSB.ca>
2024-10-07 08:51:48 -0400

    doing part 3

--- Game1.cs
+++ Game1.cs
@@ -3,7 +3,6 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 
 namespace Topic_2___Loops_and_Lists
 {
@@ -14,6 +13,9 @@ namespace Topic_2___Loops_and_Lists
         private SpriteBatch _spriteBatch;
 
         Random generator = new Random();
+
+        float seconds;
+        float respawnTime;
 
         Texture2D spaceBackgroundTexture;
         Rectangle windowRect;
@@ -31,15 +33,14 @@ namespace Topic_2___Loops_and_Lists
 
         protected override void Initialize()
         {
+            seconds = 0f;
+            respawnTime = 3f;
+
+
             windowRect = new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight);
             textures = new List<Texture2D>();
             planetTextures = new List<Texture2D>();
             planetRects = new List<Rectangle>();
-
-            for (int i = 0; i < 30; i++)
-            {
-                planetRects.Add(new Rectangle(generator.Next(windowRect.Width - 25), generator.Next(windowRect.Height - 25), 25, 25));
-            }
 
             base.Initialize();
         }
@@ -49,11 +50,10 @@ namespace Topic_2___Loops_and_Lists
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             spaceBackgroundTexture = Content.Load<Texture2D>("Images/space_background");
 
-            for (int i = 1 ;i <= 13 ;i++)
+            for (int i = 1; i <= 13; i++)
             {
                 textures.Add(Content.Load<Texture2D>("Images/16-bit-planet" + i));
             }
-
         }
 
         protected override void Update(GameTime gameTime)
@@ -61,7 +61,16 @@ namespace Topic_2___Loops_and_Lists
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            seconds += (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (seconds > respawnTime)
+            {
+                planetTextures.Add(textures[generator.Next(textures.Count)]);
+                planetRects.Add(new Rectangle(generator.Next(windowRect.Width - 25), generator.Next(windowRect.Height - 25), generator.Next(25, 76), generator.Next(25, 76)));
+                seconds = 0f;
+            }
+
+
 
             base.Update(gameTime);
         }
@@ -69,9 +78,15 @@ namespace Topic_2___Loops_and_Lists
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(spaceBackgroundTexture, windowRect, Color.White);
 
-            // TODO: Add your drawing code here
+            for (int i = 0; i < planetRects.Count; i++)
+            {
+                _spriteBatch.Draw(planetTextures[i], planetRects[i], Color.White);
+            }
 
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
